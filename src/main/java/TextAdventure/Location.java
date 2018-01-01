a54f283f6d2a7c92f245596dd6e6fbba835ba9a6
Caleb Bartlett <coolum444@hotmail.co.uk>
2018-01-01 18:08:06 +0000

    Start to add location reading
    All of this comes with the caveat of me not knowing how I'm going to set this out.

--- src/main/java/TextAdventure/Location.java
+++ src/main/java/TextAdventure/Location.java
@@ -1,6 +1,35 @@
 package TextAdventure;
+
+import java.io.FileReader;
+import java.io.IOException;
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonValue;
+
 public class Location {
 	public String name;
 	public String id;
+	private JsonValue rawJson;
+	private JsonObject raw;	
 	public Item[] items;
+	
+	public Location(String filename) throws IOException {
+		
+		// Read the file
+		FileReader file = new FileReader(filename);
+		rawJson = Json.parse(file);
+		file.close();
+		raw = rawJson.asObject();
+		
+		// Get the name from it
+		name = raw.get("name").asString();
+		
+		// Get its ID
+		id = raw.get("id").asString();
+	}
+	
+	public String getDescription() {
+		String description = raw.get("description").asString();
+		return description;
+	}
 }
