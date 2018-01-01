805465bb70459818a2fd90b1ba8fb27bae38d5cf
Caleb Bartlett <coolum444@hotmail.co.uk>
2018-01-01 17:42:36 +0000

    Start adding interpreter system
    This is the first set of code that allows the program to determine a game path so it can later read the game's files, as well as a good starting point for reading the commands from the user.

--- src/main/java/TextAdventure/TextAdventure.java
+++ src/main/java/TextAdventure/TextAdventure.java
@@ -1,16 +1,43 @@
 package TextAdventure;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class TextAdventure {
+	
+	public static String gamePath;
+	public static Location currentLocation;
+	public static boolean firstRun;  // Whether or not it's the first time you enter a room
+	public static Item[] inventory;
+	
 	public static void main(String[] args) throws IOException {
+		
+		// Nice output text to start with
 		System.out.println("Welcome to the Text Adventure thingy");
-		// Item x = new Item("test.json");
-		// System.out.println(x.name);
-
 		Scanner in = new Scanner(System.in);
+		
+		// Determine the game path
 		while (true) {
-			String line = in.nextln();
+			System.out.println("What game do you want to run?");
+			String gameName = in.nextLine();
+			load(gameName);
+			break;
 		}
+		
+		// Determine the next thing the user says
+		while (true) {
+			String line = in.nextLine();
+			if (line.equals("exit")) {
+				System.out.println("Exiting program.");
+				break;
+			}
+		}
+		in.close();
+	}
+	
+	public static void load(String gameName) {
+		File file = new File("./" + gameName);
+		gamePath = file.getAbsolutePath();
 	}
 }
